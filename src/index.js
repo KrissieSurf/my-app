20dc58caf53aa9016f8ede2ac8a97cd6f6a90f0e
KrissieSurf <krissie@kollectify.com>
2022-12-05 14:31:45 +0000

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import WeatherSearch from "./WeatherSearch";
-
+import reportWebVitals from "./reportWebVitals";
 import "./App.css";
 
 const rootElement = document.getElementById("root");
